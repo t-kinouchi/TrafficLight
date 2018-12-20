7304883b78dbc95a2bd4a1df5dd3eb03590557b4
t-kinouchi <tmk.k.0510@gmail.com>
2018-12-20 23:27:17 +0900

    main.js added

--- js/main.js
+++ js/main.js
@@ -24,8 +24,6 @@ $(function (){
 
 $(function (){
   // ②ボタンを押すと2秒毎に色が変わっていく動作
-<<<<<<< HEAD
-=======
   const promise = function() {
     return new Promise(function(resolve) {
       setTimeout(function() {
@@ -34,43 +32,12 @@ $(function (){
     });
   };
 
->>>>>>> develop
   $('#start').click(function() {
     $('#start').prop('disabled', true);
     $('#Green').prop('disabled', true);
     $('#Yellow').prop('disabled', true);
     $('#Red').prop('disabled', true);
     $('#Reset').prop('disabled', true);
-<<<<<<< HEAD
-
-    new Promise(function(resolve) {
-      setTimeout(function() {
-      $('.GreenLight').css('background', 'blue');
-      $('.YellowLight').css('background', 'black');
-      $('.RedLight').css('background', 'black');
-      }, 100);
-    })
-
-    Promise.resolve()
-    .then(function(resolve) {
-      setTimeout(function() {
-      $('.GreenLight').css('background', 'black');
-      $('.YellowLight').css('background', 'yellow');
-      $('.RedLight').css('background', 'black');
-      }, 2000);
-    })
-
-    .then(function(rsolve) {
-      setTimeout(function() {
-      $('.GreenLight').css('background', 'black');
-      $('.YellowLight').css('background', 'black');
-      $('.RedLight').css('background', 'red');
-      }, 4000);
-    })
-
-    .then(function(resolve) {
-      setTimeout(function() {
-=======
     $('.GreenLight').css('background', 'blue');
     $('.YellowLight').css('background', 'black');
     $('.RedLight').css('background', 'black');
@@ -90,7 +57,6 @@ $(function (){
     })
 
     .then(function() {
->>>>>>> develop
       $('.GreenLight').css('background', 'black');
       $('.YellowLight').css('background', 'black');
       $('.RedLight').css('background', 'black');
@@ -99,11 +65,7 @@ $(function (){
       $('#Yellow').prop('disabled', false);
       $('#Red').prop('disabled', false);
       $('#Reset').prop('disabled', false);
-<<<<<<< HEAD
-      }, 6000);
-=======
       return promise();
->>>>>>> develop
     });
   });
 });
