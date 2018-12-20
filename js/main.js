cda105e0773e41bf1f60e31d7d9badba4b18b8f1
t-kinouchi <tmk.k.0510@gmail.com>
2018-12-20 18:55:16 +0900

    index.html,main.js added

--- js/main.js
+++ js/main.js
@@ -1,34 +1,35 @@
-$(function (){    //①の処理
-  $('#Green').click(function() {
+$(function (){
+  // ①ボタンを押すたびに色が変わる動作
+  $('.Green').click(function() {
     $('.GreenLight').css('background', 'blue');
     $('.YellowLight').css('background', 'black');
     $('.RedLight').css('background', 'black');
   })
-  $('#Yellow').click(function() {
+  $('.Yellow').click(function() {
     $('.GreenLight').css('background', 'black');
     $('.YellowLight').css('background', 'yellow');
     $('.RedLight').css('background', 'black');
   })
-  $('#Red').click(function() {
+  $('.Red').click(function() {
     $('.GreenLight').css('background', 'black');
     $('.YellowLight').css('background', 'black');
     $('.RedLight').css('background', 'red');
   })
-  $('#Reset').click(function() {
+  $('.Reset').click(function() {
     $('.GreenLight').css('background', 'black');
     $('.YellowLight').css('background', 'black');
     $('.RedLight').css('background', 'black');
   })
-  });
+});
 
-
-$(function (){    //②の処理
-  $('#start').click(function() {
-    $('#start').prop('disabled', true);
-    $('#Green').prop('disabled', true);
-    $('#Yellow').prop('disabled', true);
-    $('#Red').prop('disabled', true);
-    $('#Reset').prop('disabled', true);
+$(function (){
+  // ②ボタンを押すと2秒毎に色が変わっていく動作
+  $('.start').click(function() {
+    $('.start').prop('disabled', true);
+    $('.Green').prop('disabled', true);
+    $('.Yellow').prop('disabled', true);
+    $('.Red').prop('disabled', true);
+    $('.Reset').prop('disabled', true);
     setTimeout(function() {
       $('.GreenLight').css('background', 'blue');
       $('.YellowLight').css('background', 'black');
@@ -48,11 +49,11 @@ $(function (){    //②の処理
       $('.GreenLight').css('background', 'black');
       $('.YellowLight').css('background', 'black');
       $('.RedLight').css('background', 'black');
-      $('#start').prop('disabled', false);
-      $('#Green').prop('disabled', false);
-      $('#Yellow').prop('disabled', false);
-      $('#Red').prop('disabled', false);
-      $('#Reset').prop('disabled', false);
+      $('.start').prop('disabled', false);
+      $('.Green').prop('disabled', false);
+      $('.Yellow').prop('disabled', false);
+      $('.Red').prop('disabled', false);
+      $('.Reset').prop('disabled', false);
     }, 6000);
-    })
-  });
+  })
+});
