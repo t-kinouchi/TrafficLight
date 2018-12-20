0365b94d9b32d3964e2299de8823e34ec2956b86
t-kinouchi <tmk.k.0510@gmail.com>
2018-12-20 23:24:28 +0900

    main.js added

--- js/main.js
+++ js/main.js
@@ -24,12 +24,24 @@ $(function (){
 
 $(function (){
   // ②ボタンを押すと2秒毎に色が変わっていく動作
+<<<<<<< HEAD
+=======
+  const promise = function() {
+    return new Promise(function(resolve) {
+      setTimeout(function() {
+        resolve();
+      }, 2000);
+    });
+  };
+
+>>>>>>> develop
   $('#start').click(function() {
     $('#start').prop('disabled', true);
     $('#Green').prop('disabled', true);
     $('#Yellow').prop('disabled', true);
     $('#Red').prop('disabled', true);
     $('#Reset').prop('disabled', true);
+<<<<<<< HEAD
 
     new Promise(function(resolve) {
       setTimeout(function() {
@@ -58,6 +70,27 @@ $(function (){
 
     .then(function(resolve) {
       setTimeout(function() {
+=======
+    $('.GreenLight').css('background', 'blue');
+    $('.YellowLight').css('background', 'black');
+    $('.RedLight').css('background', 'black');
+
+    promise().then(function() {
+      $('.GreenLight').css('background', 'black');
+      $('.YellowLight').css('background', 'yellow');
+      $('.RedLight').css('background', 'black');
+      return promise();
+    })
+
+    .then(function() {
+      $('.GreenLight').css('background', 'black');
+      $('.YellowLight').css('background', 'black');
+      $('.RedLight').css('background', 'red');
+      return promise();
+    })
+
+    .then(function() {
+>>>>>>> develop
       $('.GreenLight').css('background', 'black');
       $('.YellowLight').css('background', 'black');
       $('.RedLight').css('background', 'black');
@@ -66,7 +99,11 @@ $(function (){
       $('#Yellow').prop('disabled', false);
       $('#Red').prop('disabled', false);
       $('#Reset').prop('disabled', false);
+<<<<<<< HEAD
       }, 6000);
+=======
+      return promise();
+>>>>>>> develop
     });
   });
 });
